528ac7d4a408215968ea3b161a4485c9e95055e8
refcell <abigger87@gmail.com>
2024-07-25 07:30:07 -0400

    fix: use raw tx data

--- crates/op-test-vectors/src/derivation.rs
+++ crates/op-test-vectors/src/derivation.rs
@@ -1,8 +1,7 @@
 //! Module containing the derivation test fixture.
 
 use alloy::consensus::Blob;
-use alloy::primitives::B256;
-use anvil_core::eth::transaction::TypedTransaction;
+use alloy::primitives::{Bytes, B256};
 use serde::{Deserialize, Serialize};
 
 /// The derivation fixture is the top-level object that contains
@@ -26,7 +25,7 @@ pub struct FixtureBlock {
     pub timestamp: u64,
     /// Block Transactions.
     /// EIP-2718 encoded raw transactions
-    pub transactions: Vec<TypedTransaction>,
+    pub transactions: Vec<Bytes>,
     /// Blobs for this block.
     pub blobs: Vec<Box<Blob>>,
 }
@@ -34,7 +33,7 @@ pub struct FixtureBlock {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use alloy::primitives::b256;
+    use alloy::primitives::{bytes, b256};
 
     fn ref_blocks() -> Vec<FixtureBlock> {
         vec![
@@ -42,21 +41,30 @@ mod tests {
                 number: 1,
                 hash: b256!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"),
                 timestamp: 102,
-                transactions: vec![],
+                transactions: vec![
+                    bytes!("02f870018307c100808476d0a39c82565f94388c818ca8b9251b393131c08a736a67ccb1929787b60572b2eb6c9080c001a033bee682348fa78ffc1027bc9981e7dc60eca03af909c4eb05720e781fdae179a01ccf85367c246082fa09ef748d3b07c90752c2b59034a6b881cf99aca586eaf5"),
+                    bytes!("02f870018307c100808476d0a39c82565f94388c818ca8b9251b393131c08a736a67ccb1929787b60572b2eb6c9080c001a033bee682348fa78ffc1027bc9981e7dc60eca03af909c4eb05720e781fdae179a01ccf85367c246082fa09ef748d3b07c90752c2b59034a6b881cf99aca586eaf5"),
+                    bytes!("02f870018307c100808476d0a39c82565f94388c818ca8b9251b393131c08a736a67ccb1929787b60572b2eb6c9080c001a033bee682348fa78ffc1027bc9981e7dc60eca03af909c4eb05720e781fdae179a01ccf85367c246082fa09ef748d3b07c90752c2b59034a6b881cf99aca586eaf5"),
+                ],
                 blobs: vec![],
             },
             FixtureBlock {
                 number: 2,
                 hash: b256!("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"),
                 timestamp: 104,
-                transactions: vec![],
+                transactions: vec![
+                    bytes!("02f870018307c100808476d0a39c82565f94388c818ca8b9251b393131c08a736a67ccb1929787b60572b2eb6c9080c001a033bee682348fa78ffc1027bc9981e7dc60eca03af909c4eb05720e781fdae179a01ccf85367c246082fa09ef748d3b07c90752c2b59034a6b881cf99aca586eaf5"),
+                    bytes!("02f870018307c100808476d0a39c82565f94388c818ca8b9251b393131c08a736a67ccb1929787b60572b2eb6c9080c001a033bee682348fa78ffc1027bc9981e7dc60eca03af909c4eb05720e781fdae179a01ccf85367c246082fa09ef748d3b07c90752c2b59034a6b881cf99aca586eaf5"),
+                ],
                 blobs: vec![],
             },
             FixtureBlock {
                 number: 2,
                 hash: b256!("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"),
                 timestamp: 106,
-                transactions: vec![],
+                transactions: vec![
+                    bytes!("02f870018307c100808476d0a39c82565f94388c818ca8b9251b393131c08a736a67ccb1929787b60572b2eb6c9080c001a033bee682348fa78ffc1027bc9981e7dc60eca03af909c4eb05720e781fdae179a01ccf85367c246082fa09ef748d3b07c90752c2b59034a6b881cf99aca586eaf5"),
+                ],
                 blobs: vec![],
             },
         ]
