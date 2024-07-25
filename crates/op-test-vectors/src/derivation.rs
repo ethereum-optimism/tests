9df0fd322dd2c7c049d6ae1270402144159676a5
refcell <abigger87@gmail.com>
2024-07-25 07:09:59 -0400

    fix: remove transitions

--- crates/op-test-vectors/src/derivation.rs
+++ crates/op-test-vectors/src/derivation.rs
@@ -1,32 +1,16 @@
 //! Module containing the derivation test fixture.
 
-use serde::{Deserialize, Serialize};
-use alloy::primitives::B256;
 use alloy::consensus::Blob;
+use alloy::primitives::B256;
 use anvil_core::eth::transaction::TypedTransaction;
+use serde::{Deserialize, Serialize};
 
 /// The derivation fixture is the top-level object that contains
 /// everything needed to run a derivation test.
 #[derive(Serialize, Deserialize, Debug, Default, Eq, PartialEq)]
 pub struct DerivationFixture {
-    /// A list of chain transitions.
-    pub transitions: Vec<Transition>,
     /// A list of L1 Blocks to derive from.
     pub blocks: Vec<FixtureBlock>,
-}
-
-/// A transition is a change in the chain state from one block to another.
-/// Since previous blocks can be further in the past than block N-1, we
-/// encompass reorgs.
-#[derive(Serialize, Deserialize, Debug, Default, Eq, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct Transition {
-    /// The previous block number.
-    pub previous_block: u64,
-    /// The current block number.
-    pub current_block: u64,
-    /// The current block hash.
-    pub block_hash: B256,
 }
 
 /// A fixture block is a minimal block with associated data including blobs
@@ -52,26 +36,6 @@ mod tests {
     use super::*;
     use alloy::primitives::b256;
 
-    fn ref_transitions() -> Vec<Transition> {
-        vec![
-            Transition {
-                previous_block: 1,
-                current_block: 2,
-                block_hash: b256!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"),
-            },
-            Transition {
-                previous_block: 2,
-                current_block: 3,
-                block_hash: b256!("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"),
-            },
-            Transition {
-                previous_block: 3,
-                current_block: 4,
-                block_hash: b256!("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"),
-            },
-        ]
-    }
-
     fn ref_blocks() -> Vec<FixtureBlock> {
         vec![
             FixtureBlock {
@@ -80,7 +44,21 @@ mod tests {
                 timestamp: 102,
                 transactions: vec![],
                 blobs: vec![],
-            }
+            },
+            FixtureBlock {
+                number: 2,
+                hash: b256!("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"),
+                timestamp: 104,
+                transactions: vec![],
+                blobs: vec![],
+            },
+            FixtureBlock {
+                number: 2,
+                hash: b256!("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"),
+                timestamp: 106,
+                transactions: vec![],
+                blobs: vec![],
+            },
         ]
     }
 
@@ -88,7 +66,6 @@ mod tests {
     fn test_derivation_fixture() {
         let fixture_str = include_str!("./testdata/derivation_fixture.json");
         let fixture: DerivationFixture = serde_json::from_str(fixture_str).unwrap();
-        assert_eq!(fixture.transitions, ref_transitions());
         assert_eq!(fixture.blocks, ref_blocks());
     }
 
@@ -97,16 +74,12 @@ mod tests {
         let fixture_str = include_str!("./testdata/fixture_block.json");
         let fixture: FixtureBlock = serde_json::from_str(fixture_str).unwrap();
         assert_eq!(fixture.number, 1);
-        assert_eq!(fixture.hash, b256!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"));
+        assert_eq!(
+            fixture.hash,
+            b256!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+        );
         assert_eq!(fixture.timestamp, 102);
         assert_eq!(fixture.transactions.len(), 1);
         assert_eq!(fixture.blobs.len(), 0);
-    }
-
-    #[test]
-    fn test_transitions() {
-        let transitions_str = include_str!("./testdata/transitions.json");
-        let transitions: Vec<Transition> = serde_json::from_str(transitions_str).unwrap();
-        assert_eq!(transitions, ref_transitions());
     }
 }
